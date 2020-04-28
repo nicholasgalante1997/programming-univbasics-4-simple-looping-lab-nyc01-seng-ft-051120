3543bb64996b9d23bc49d160622176682817ffd8
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-28 14:12:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,7 +28,7 @@ def return_string_array(array)
   count = 0 
   while count < array.length do 
    array[count] = array[count].to_s 
-   puts array[count]
    count += 1 
  end 
+ return array 
 end
