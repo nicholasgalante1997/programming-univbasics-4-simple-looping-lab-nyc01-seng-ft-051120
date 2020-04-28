147d9f7a11909be0f2f1eda89649af60e20fd592
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-28 14:11:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,11 @@ def output_array(array)
 end 
 end 
   
+def return_string_array(array)
+  
+  count = 0 
+  while count < array.length do 
+   array[count] = array[count].to_s 
+   puts array[count]
+ end 
+end
