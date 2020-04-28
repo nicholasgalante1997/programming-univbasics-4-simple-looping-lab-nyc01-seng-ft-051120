c38189043a79317b270755114d9d91b1a2db91df
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-28 14:02:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,10 +15,10 @@ def loop_message_n_times(string, integer)
 end 
 end 
 
-def output_array(array, number)
+def output_array(message, number)
   count = 0 
   until count = number
-  puts array[number]
+  puts message 
   count += 1 
 end 
 end 
